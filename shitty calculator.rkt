d83e3db5cca01edee6a8b01a398564553693b0ae
Brian Dinh <bdinh98@gmail.com>
2019-09-02 13:06:10 -0400

    prototyped calculator in racket. dont judge me pls

---
+++ shitty calculator.rkt
@@ -0,0 +1,45 @@
+#lang racket
+
+(define (calculatorLoop)
+  (display "shitty calculator 3000 (+,-,*, /, exit)")
+  (define command (read-line))
+  (cond [(string-contains? command "+") (add)(calculatorLoop)]
+        [(string-contains? command "-") (subtract)(calculatorLoop)]
+        [(string-contains? command "*") (multiply)(calculatorLoop)]
+        [(string-contains? command "/") (divide)(calculatorLoop)]
+        [(string-contains? command "exit")  (displayln "Goodbye!")]        
+        [else (displayln "UNKNOWN") (calculatorLoop)]
+        )
+ )
+
+(define (add)
+  (define numb1 (read-line))
+  (define numb2 (read-line))
+  (define printNumb (+ (string->number numb1) (string->number numb2)))
+  (displayln(~v printNumb))
+)
+
+(define (subtract)
+  (define numb1 (read-line))
+  (define numb2 (read-line))
+  (define printNumb (-(string->number numb1) (string->number numb2)))
+  (displayln(~v printNumb))
+)
+
+(define (multiply)
+  (define numb1 (read-line))
+  (define numb2 (read-line))
+  (define printNumb (* (string->number numb1) (string->number numb2)))
+  (displayln(~v printNumb))
+)
+
+(define (divide)
+  (define numb1 (read-line))
+  (define numb2 (read-line))
+  (define printNumb (/ (string->number numb1) (string->number numb2)))
+  (displayln(~v printNumb))
+)
+
+(define (start)(calculatorLoop))
+
+(start)
